acd9b75fde3ed2f71ebaffd196a2913addda8603
Kerlos Magdy <kerlosssony@gmail.com>
2025-02-07 15:38:50 +0200

    fix responsive for home page

--- src/components/sectionFive/sectionFive.tsx
+++ src/components/sectionFive/sectionFive.tsx
@@ -7,19 +7,19 @@ export default function SectionFive() {
     return (
         <div className='mx-[8px] w-[calc(100%-16px)] rounded-[25px] relative pb-0 p-[40px]
         !bg-main my-[10px] overflow-hidden
-        flex flex-col mb-[80px] lg:flex-row items-center gap-[0px] lg:gap-[100px] 2xl:gap-[240px] justify-center
+        flex flex-col mb-[80px] xl:flex-row items-center gap-[0px] lg:gap-[100px] 2xl:gap-[240px] justify-center
         '>
             <Background linesOnly />
             <div className='flex flex-col pb-[10px] md:pb-[40px] mt-[40px] xl:mt-0'>
-                <span className='font-medium mx-auto lg:mx-0 text-[13px] font-white mb-[16px] text-white'>EASE View</span>
-                <span className='font-medium text-center lg:text-start text-[20px] md:text-[39px] font-white max-w-[100%] xl:max-w-[491px] text-white mb-[24px]'>You can now find an easy-breezy job search</span>
-                <span className='font-[400] text-center lg:text-start text-[13px] md:text-[20px] text-[#E6E6E6] max-w-[100%] lg:max-w-[491px] 
+                <span className='font-medium mx-auto xl:mx-0 text-[13px] font-white mb-[16px] text-white'>EASE View</span>
+                <span className='font-medium text-center xl:text-start text-[20px] md:text-[39px] font-white max-w-[100%] xl:max-w-[491px] text-white mb-[24px]'>You can now find an easy-breezy job search</span>
+                <span className='font-[400] text-center xl:text-start text-[13px] md:text-[20px] text-[#E6E6E6] max-w-[100%] xl:max-w-[491px] 
                 mb-[20px] md:mb-[59px]'>If you`re seeking opportunities for employment, internships, or networking, our job fair application simplifies the process of connecting with potential employers and discovering your ideal career path all from the comfort of your device.</span>
-                <Link href='/' className='flex flex-col mx-auto lg:mx-0 shadow-xl shadow-white/20 items-center justify-center py-[18.32px] px-[32.57px] bg-white text-black rounded-[12.5px] font-medium text-[17px] md:text-[25px] w-fit'>
+                <Link href='/' className='flex flex-col mx-auto xl:mx-0 shadow-xl shadow-white/20 items-center justify-center py-[18.32px] px-[32.57px] bg-white text-black rounded-[12.5px] font-medium text-[17px] md:text-[25px] w-fit'>
                     Get Exploor now!
                 </Link>
             </div>
-            <Image width={700} height={700} src={'/assets/images/sectionFive.png'} alt='background' className='mt-0 lg:mt-52 xl:mt-20' />
+            <Image width={700} height={700} src={'/assets/images/sectionFive.png'} alt='background' className='mt-0 xl:mt-20' />
         </div>
     )
 }
